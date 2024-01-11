ab3b65bc386fafb5ced808b587260365a766c5b9
Samu <samu@samumoila.net>
2024-01-11 22:02:57 +0200

    ...

---
+++ java/testi2/src/TervetuloaViestiDialogi.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class TervetuloaViestiDialogi {
+    public static void kutsu () {
+        JOptionPane.showMessageDialog(null,
+                "Tervetuloa Javaan!",
+                "Näytä viesti",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+}

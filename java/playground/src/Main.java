998b46cdc5beae97d4f73345701f6b82627c7621
Samu <samu@samumoila.net>
2024-01-17 12:35:03 +0200

    ...

--- java/playground/src/Main.java
+++ java/playground/src/Main.java
@@ -1,16 +1,4 @@
-import javax.swing.*;
-import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!\n");
-        System.out.println("Anna suorakulmion mitat kokonaislukuna:\n");
-        Scanner input = new Scanner(System.in);
-        int input1 = input.nextInt();
-        int input2 = input.nextInt();
-        Laskuri olioLaskuri = new Laskuri(input1,input2);
-
-        System.out.println(olioLaskuri.pintaAla());
+    public static void main(String [] args) {
     }
 }

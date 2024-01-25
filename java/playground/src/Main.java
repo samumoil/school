394f956959dc8cccd2808d1805c7f4d9c5d3da80
samumoil <samumoil@peikkoposti.com>
2024-01-25 15:41:40 +0200

    ...

--- java/playground/src/Main.java
+++ java/playground/src/Main.java
@@ -1,27 +1,15 @@
-// ########## d-kohta ##########
-public class Main{
-        public static void main (String [] args) {
-// ########## d i-kohta ##########
-                Tili tili1 = new Tili(123, 123.12);
-                Tili tili2 = new Tili(234, 234.23);
-                Tili tili3 = new Tili(tili1);
-
-// ########## d ii-kohta ##########
-                System.out.println(tili1);
-                System.out.println(tili2);
-                System.out.println(tili3);
-
-// ########## d iii-kohta ##########
-                System.out.println("Ovatko tilit 1 ja 2 samoja? " + tili1.equals(tili2));
-                System.out.println("Ovatko tilit 1 ja 3 samoja? " + tili1.equals(tili3));
-                System.out.println("Ovatko tilit 2 ja 3 samoja? " + tili2.equals(tili3));
-
-// ########## d iv-kohta ##########
-// Jos tileillä on saman verran rahaa, jää voimaan se, joka oli "suurin" ensimmäisenä.
-                Tili suurin = tili1;
-                if (tili2.suurempiKuin(suurin)){  suurin = tili2;  }
-                if (tili3.suurempiKuin(suurin)){  suurin = tili3;  }
-                System.out.println("Eniten rahaa on tilillä: " + suurin);
+public class Main {
+        static int i = 0;
+        static int j = 0;
+        public static void main(String [] args) {
+                int i = 2;
+                int j = 3;
+                {
+                        j = 1;
+                        System.out.println("i + j on " + i + j);
+                }
+                int k = i + j;
+                System.out.println("k on " + k);
+                System.out.println("j on " + j);
         }
 }
-

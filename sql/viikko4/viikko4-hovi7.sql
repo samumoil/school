1c773f410d8da499e494f5372858b05c8aa3952b
samumoil <samumoil@peikkoposti.com>
2024-02-09 19:32:21 +0200

    ...

---
+++ sql/viikko4/viikko4-hovi7.sql
@@ -0,0 +1,49 @@
+-- WINDOW FUNKTIOT
+-- Tehtävä 1: Laske jokaiselle henkilölle heidän palkkansa suhde osastonsa keskimääräiseen palkkaan.
+
+
+-- Tehtävä 2: Määritä jokaisen projektin sijoitus sen prioriteetin perusteella.
+-- Korkein prioriteetti tulee sijoituksessa ensin.
+
+
+-- Tehtävä 3: Hae henkilöiden tekemät työtunnit kuhunkin projektiin ja
+-- näytä erotus sen ja kyseisen projektiin keskiarvoisesti tehtyjen tuntien välillä.
+-- Näytä henkilöiden sijoitus erotuksen perusteella, suurin erotus ensin
+
+
+
+-- TRIGGERIT
+
+-- Tehtävä 1: Luo triggeri palkan tarkistusta varten.
+-- palkan on oltava välillä 1000-10000
+-- testaa triggeri
+
+
+-- Tehtävä 2: Luo triggeri tunnit_suun päivitystä varten.
+-- ennen päivitystä jos päityksessä asetetaan uusi tunnit arvo, niin 
+-- aseta tunnit_suun = vanha tunnit arvo
+
+
+-- PROSEDUURIT
+
+-- Tehtävä 1: Proseduuri keskimääräisen palkan laskemiseksi tietylle osastolle.
+-- testaa
+
+
+-- Tehtävä 2: Proseduuri uuden projektin lisäämiseksi.
+-- Proseduuri saa parametreina projekti-taulun sarakkeita vastaavat arvot
+-- ja lisää rivin niiden mukaan
+
+
+-- FUNKTIOT
+
+-- Tehtävä 1: Funktio veron jälkeisen palkan eli nettopalkan laskemiseksi.
+-- argumenttina htun, paluuarvona nettopalkka
+
+
+
+-- Tehtävä 2: Funktio projektissa käytettyjen tuntien kokonaismäärän laskemiseksi.
+
+
+
+

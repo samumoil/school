b1bf0d87e1f82a3befee097cad13cb2704d5f8d9
samumoil <samumoil@peikkoposti.com>
2024-07-15 14:42:16 +0300

    ...

--- android/lainalaskuri/app/src/main/java/net/samumoila/lainalaskuri_mallin_mukaan/MainActivity.java
+++ android/lainalaskuri/app/src/main/java/net/samumoila/lainalaskuri_mallin_mukaan/MainActivity.java
@@ -1,6 +1,9 @@
 package net.samumoila.lainalaskuri_mallin_mukaan;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -12,10 +15,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Referenssi liukuvalintaan.
-    private SeekBar sbKorkovalinta = findViewById(R.id.sbKoronValinta);
-    // Referenssi korkonäyttöön.
-    private TextView korkonaytto = findViewById(R.id.tvKorkoprosentti);
+    // Muuttuja liukuvalintaan.
+    private SeekBar sbKorkovalinta;
+    // Muuttuja korkonäyttöön.
+    private TextView korkonaytto;
+    // Muuttuja lainamäärän kenttään.
+    private EditText lainamaara_kentta;
+    // Muuttuja lainan keston kenttään.
+    private EditText lainankesto_kentta;
+    //
+    private TextView selitystekstikentta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +37,28 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        String strSelitysteksti = getString(R.string.selitysteksti_upper);
+        String strLainanMaara = getString(R.string.lainamaara_teksti);
+        String strLainanKesto = getString(R.string.lainankesto_teksti);
+        String strKorko = getString(R.string.korkonaytto);
+
+        // Nämä voidaan referoida vasta tässä vaiheessa!!!
+        // Rererenssi liukuvalintaan.
+        sbKorkovalinta = findViewById(R.id.sbKoronValinta);
+        // Referenssi korkonäyttöön.
+        korkonaytto = findViewById(R.id.tvKorkoprosentti);
+        //
+        lainamaara_kentta = findViewById(R.id.etLainamaara);
+        //
+        lainankesto_kentta = findViewById(R.id.etLainanKesto);
+        //
+        selitystekstikentta = findViewById(R.id.tvTulokset);
+
         sbKorkovalinta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                paivitaKorko();
+                paivitaKorkoteksti();
+                paivitaSelitysteksti();
             }
 
             @Override
@@ -43,13 +70,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Luodaan tekstimuutoksen kuuntelija.
+        TekstiVahti tekstiVahti = new TekstiVahti();
+        // Osoitetaan kumpikin tekstikenttä käyttämään tätä kuuntelijaoliota.
+        lainamaara_kentta.addTextChangedListener(tekstiVahti);
+        lainankesto_kentta.addTextChangedListener(tekstiVahti);
 
 
-    public void paivitaKorko(){
-        String lokalisoituKorkoteksti = getString(R.string.korkonaytto);
-        String korkoprosentti = String.format("%d %%", sbKorkovalinta.getProgress());
-        korkonaytto.setText(lokalisoituKorkoteksti + korkoprosentti);
     }
 
+    void paivitaKorkoteksti(){
+        String lokalisoituKorkoteksti = getString(R.string.korkonaytto);
+//        String korkoprosentti = String.format("%d %%", sbKorkovalinta.getProgress());
+        String korkoprosentti = String.valueOf(sbKorkovalinta.getProgress());
+        String naytettavaTeksti = lokalisoituKorkoteksti + " " + korkoprosentti + " %";
+        korkonaytto.setText(naytettavaTeksti);
+    }
+
+    private class TekstiVahti implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            paivitaSelitysteksti();
+        }
+    }
+
+    void paivitaSelitysteksti() {
+        String naytettavaTeksti = String.format("%s/n", )
+        try {
+            double lainanmaara = Double.parseDouble(lainamaara_kentta.getText().toString());
+            int lainankesto = Integer.getInteger(lainankesto_kentta.getText().toString());
+            int korko = sbKorkovalinta.getProgress();
+            double normalisoituKorko = korko / 100d;
+
+            double kokonaisKorko = lainanmaara * normalisoituKorko * lainankesto/12;
+            double kokonaismaara = lainanmaara + kokonaisKorko;
+
+
+
+        } catch (NumberFormatException e) {
+
+        }
     }
 }
